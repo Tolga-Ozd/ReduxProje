ab61736914766a442261118578a34589e7e195d5
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-23 14:43:04 +0300

    cartSlice

--- src/control/cartSlice.js
+++ src/control/cartSlice.js
@@ -11,4 +11,6 @@ const cartSlice = createSlice({
     initialState,
 })
 
+console.log(cartSlice)
+
 export default cartSlice.reducer;
--- src/store.jsx
+++ src/store.jsx
@@ -1,8 +1,9 @@
 import { configureStore, createReducer } from "@reduxjs/toolkit";
+import cartReducer from "./control/cartSlice"
 
 export const store = configureStore({
     reducer: {
-        cart:createReducer
+        cart:cartReducer ,
     },
         
 });
