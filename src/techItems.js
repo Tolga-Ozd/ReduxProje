fcce95969f56b914a45abdacca57ca481331b31b
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-23 18:01:49 +0300

    techItems

---
+++ src/techItems.js
@@ -0,0 +1,32 @@
+const techItems = [
+    {
+        id: '1',
+        title: 'Monster NoteBook',
+        price: '36299',
+        img: 'https://img-monsternotebook.mncdn.com/UPLOAD/urun-gorselleri-yeni/ABRA/A5_V20/thumb/A5_V20_i7_1_large.png',
+        quantity: 1,
+      },
+      {
+        id: '2',
+        title: 'Pusat 7.1 Surroun Kulaklık',
+        price: '999',
+        img: 'https://img-monsternotebook.mncdn.com/UPLOAD/urun-gorselleri-yeni/PUSAT/7.1lite-rework/thumb/1_large.png',
+        quantity: 1,
+      },
+      {
+        id: '3',
+        title: 'Mekanik Oyuncu Klavyesi',
+        price: '1899',
+        img: 'https://img-monsternotebook.mncdn.com/UPLOAD/urun-gorselleri-yeni/Aksesuarlar/PUSAT/K3-klavye/kucuk-resimler/Yeni_render/thumb/TR_K3_7_medium.png',
+        quantity: 1,
+      },
+      {
+        id: '4',
+        title: 'Pusat V5 Oyuncu Mouse',
+        price: '899 ',
+        img: 'https://img-monsternotebook.mncdn.com/UPLOAD/aksesuarlar-yeni/Pusat-V5/1.png',
+        quantity: 1,
+      },
+]
+
+export default techItems;
