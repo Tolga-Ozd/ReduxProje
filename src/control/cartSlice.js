29ddffc6198ed87c5089501fbba12871dcce38ab
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-26 08:57:01 +0300

    media

--- src/control/cartSlice.js
+++ src/control/cartSlice.js
@@ -1,52 +1,56 @@
-import { createSlice } from "@reduxjs/toolkit"; 
+import { createSlice } from "@reduxjs/toolkit";
 import techItems from "../techItems";
 
 const initialState = {
-    cartItems: techItems ,
-    quantity : 5 ,
-    total : 0,
+    cartItems: techItems,
+    quantity: 5,
+    total: 0,
 };
 
-console.log(techItems)
+const showAlert = (message) => {
+    alert(message);
+};
 
 const cartSlice = createSlice({
-    name: "cart" ,
+    name: "cart",
     initialState,
     reducers: {
-        clearCart: (state)=>{
+        clearCart: (state) => {
             state.cartItems = [];
         },
-        removeItem :(state , aciton) => {
-            const itemId = aciton.payload;
-            state.cartItems = state.cartItems.filter((item) => item.id !== itemId)
+        removeItem: (state, action) => {
+            const itemId = action.payload;
+            state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
         },
-        increase : (state , action) => {
-            const cartItem = state.cartItems.find(
-                (item) =>item.id === action.payload
-            );
-            cartItem.quantity += 1;
-         },
-         decrease : (state , action) => {
-            const cartItem = state.cartItems.find(
-                (item) =>item.id === action.payload
-            );
-            cartItem.quantity -= 1;
-         },
-         calculateTotal : (state) =>{
-            let total = 0 ;
-            let quantity = 0 ;
-            state.cartItems.forEach((item)=>{
+        increase: (state, action) => {
+            const cartItem = state.cartItems.find((item) => item.id === action.payload);
+            if (cartItem.quantity < 5) {
+                cartItem.quantity += 1;
+            } else {
+                showAlert("Ürün sayısı 5'ten fazla olamaz!");
+            }
+        },
+        decrease: (state, action) => {
+            const cartItem = state.cartItems.find((item) => item.id === action.payload);
+            if (cartItem.quantity >=1) {
+                cartItem.quantity -= 1;
+            } else {
+                showAlert("Ürün sayısı 0'dan düşük olamaz!");
+            }
+        },
+        calculateTotal: (state) => {
+            let total = 0;
+            let quantity = 0;
+            state.cartItems.forEach((item) => {
                 total += item.quantity * item.price;
                 quantity += item.quantity;
-            })
-            state.quantity = quantity ;
-            state.total = total ;
-         }
+            });
+            state.quantity = quantity;
+            state.total = total;
+        },
     },
 });
 
-// console.log(cartSlice)
-
-export  const { clearCart ,removeItem ,increase ,decrease ,calculateTotal } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, calculateTotal } = cartSlice.actions;
 
 export default cartSlice.reducer;
